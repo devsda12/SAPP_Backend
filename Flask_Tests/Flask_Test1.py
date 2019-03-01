68f87241037857f759cce97e46bfd9e78a36d357
devsda12 <jmbalster@hotmail.com>
2019-03-01 11:52:32 +0100

    New naming for the test function

--- Flask_Tests/Flask_Test1.py
+++ Flask_Tests/Flask_Test1.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/testshake")
-def hello_world():
-    return "Hello World"
+def testshake():
+    return "Testshake Succesfull"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
