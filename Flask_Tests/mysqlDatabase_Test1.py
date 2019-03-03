891f0672c46ccc9c38fdcc53d9bebdbc6e1052f2
devsda12 <jmbalster@hotmail.com>
2019-03-03 15:02:28 +0100

    Small database test change

--- Flask_Tests/mysqlDatabase_Test1.py
+++ Flask_Tests/mysqlDatabase_Test1.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
-database = mysql.connector.connect(host="localhost", user="root", passwd="SAPP")
+database = mysql.connector.connect(host="localhost", user="sapp_user", passwd="SAPP-PASSWORD_F0R-YOU")
 
 cursor = database.cursor()
 
-cursor.execute("SHOW DATABASES")
+cursor.execute("SHOW TABLES")
 
 for x in cursor:
     print(x)
