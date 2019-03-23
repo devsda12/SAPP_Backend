7265d7ea0c864c52bf9ea52d701dbe3dc9e7b86e
devsda12 <jmbalster@hotmail.com>
2019-03-23 17:33:37 +0100

    latest test for server

--- database_Handlers.py
+++ database_Handlers.py
@@ -209,6 +209,8 @@ class database_Handlers:
 
         self.sapp_cursor.execute('SELECT * FROM ' + requestconv_id + ';')
         result = self.sapp_cursor.fetchall()
+        print(len(result))
+        print(result)
 
         for item in result:
             chatDict[item[0]] = [item[1], item[2], item[3]]
