93aaa1cc0bc5fdb9c17e5c30c6044153e34beaad
remon <remonhoogendijk@cs,com>
2019-04-02 13:40:09 +0200

    fixed addStat get date

--- database_Handlers.py
+++ database_Handlers.py
@@ -310,14 +310,7 @@ class database_Handlers:
         resultStatstWeek = self.sapp_cursor.fetchone()
         resultCurrentWeek = str(datetime.date.today().isocalendar()[1])
 
-        print(resultStatstWeek[0])
-        print(resultCurrentWeek)
-        print(resultStatstWeek[0] != resultCurrentWeek)
-        print(type(resultCurrentWeek))
-        print(type(resultStatstWeek[0]))
-
-        if resultStatstWeek[0] != resultCurrentWeek:
-            print(resultStatstWeek[0] != resultCurrentWeek)
+        if str(resultStatstWeek[0]) != resultCurrentWeek:
             self.sapp_cursor.execute('UPDATE StatsTable SET logins = 0;')
             self.sapp_database.commit()
             self.sapp_cursor.execute('UPDATE StatsTable SET messages = 0;')
