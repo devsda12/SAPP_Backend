eb7dcb800047135ec1e431256fc329a619960beb
devsda12 <jmbalster@hotmail.com>
2019-05-07 15:33:09 +0200

    Index problem fixed, COST US HALF A DAY TO FIGURE THIS OUT XD

--- flask_Main.py
+++ flask_Main.py
@@ -130,7 +130,7 @@ class flask_Main:
                         for item in chatResult:
                             returnstring = returnstring + '{table_Name:"' + item + '", partner_Id:"' + str(chatResult[item][0]) + '", partner_Username:"' + str(chatResult[item][1]) + '", partner_Quote:"' + str(chatResult[item][2]) + '", last_Message:"' + str(chatResult[item][3]) + '", message_Sender:"' + str(chatResult[item][4]) + '", message_Date:"' + str(chatResult[item][5]) + '", newProfilePictureId:"' + str(chatResult[item][6]) + '"},'
                             if chatResult[item][5] != "null":
-                                self.profilePicsToBeRequested.append(chatResult[item][5])
+                                self.profilePicsToBeRequested.append(chatResult[item][6])
                         returnstring = returnstring[:-1]
                         returnstring = returnstring + "]"
                         return returnstring
